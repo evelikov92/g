b889dafa31509f5c543312568cea718d932f28ee
evelikov92 <evelikov92@gmail.com>
2019-10-03 16:32:07 +0300

    Implement logging system

---
+++ Zong/src/Zong/log/z_log.cpp
@@ -0,0 +1,37 @@
+#include "z_log.h"
+
+
+namespace Zong
+{
+	namespace Log
+	{
+		std::shared_ptr<spdlog::logger> Z_Log::s_Core_Logger;
+		std::shared_ptr<spdlog::logger> Z_Log::s_Client_Logger;
+
+		void Z_Log::init()
+		{
+			spdlog::set_pattern("%^[%T] %n: %v%$");
+
+			s_Core_Logger = spdlog::stdout_color_mt("Zong");
+			s_Core_Logger->set_level(spdlog::level::trace);
+
+			s_Client_Logger = spdlog::stderr_color_mt("Game");
+			s_Client_Logger->set_level(spdlog::level::trace);
+		}
+
+		void Z_Log::write(const char* msg)
+		{
+
+		}
+
+		inline std::shared_ptr<spdlog::logger>& Z_Log::get_core_logger()
+		{
+			return Z_Log::s_Core_Logger;
+		}
+		
+		inline std::shared_ptr<spdlog::logger>& Z_Log::get_client_logger()
+		{
+			return Z_Log::s_Client_Logger;
+		}
+	}
+}
--- Zong/src/Zong/z_entry_point.h
+++ Zong/src/Zong/z_entry_point.h
@@ -3,11 +3,16 @@
 #ifdef ZN_PLATFORM_WINDOWS
 
 #include "z_app.h"
+#include "log/z_log.h"
 
 extern Zong::Z_App* Zong::create_app();
 
 int main(int argc, char** argv)
 {
+	Zong::Log::Z_Log::init();
+	ZONG_CORE_WARN("initialize logger");
+	ZONG_INFO("initialize client logger");
+
 	auto app = Zong::create_app();
 	app->run();
 	delete app;
